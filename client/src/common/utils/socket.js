f7aff5eacf459aa237d76f230e5c6122f0f62981
Tukenov, Almaz <almaztukenov@tengizchevroil.com>
2022-09-29 17:21:04 +0500

    deploy

--- client/src/common/utils/socket.js
+++ client/src/common/utils/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 const PORT = 5050;
-const SERVER = "linez.kz";
-const socket = io("http://localhost:5050");
+const SERVER = "http://linez.kz";
+const socket = io(SERVER + ":5050");
 export default socket;
