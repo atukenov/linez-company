2dc5cecfd257e386ec8695b872caafa10bbfd6ef
amakenzi <almaz.t97@gmail.com>
2022-09-30 11:30:52 +0500

    fix

--- client/src/common/utils/socket.js
+++ client/src/common/utils/socket.js
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 const PORT = 5050;
 const SERVER = "http://linez.kz";
 const local = "http://localhost:5050";
-const socket = io(local);
+const socket = io(SERVER);
 export default socket;
